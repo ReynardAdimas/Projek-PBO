a6e5c915fc83e1e9bbef2c1d902be92110e66256
ReynardAdimas <dimastube22@gmail.com>
2025-05-16 16:18:26 +0700

    add main

--- src/main.java
+++ src/main.java
@@ -1,5 +1,18 @@
 public class main {
     public static void main(String[] args) {
+        System.out.println("=== Hitung Bangun Geometri ===");
+        System.out.println();
 
+        // Segitiga
+        Segitiga segitiga = new Segitiga(6,4,5,5,6);
+        System.out.println("Segitiga");
+        System.out.println("Luas: " + segitiga.hitungLuas());
+        System.out.println("Keliling: " + segitiga.hitungKeliling());
+
+        LimasSegitiga limas = new LimasSegitiga(6,4,5,5,6,8);
+        System.out.println("\nLimas Segitiga");
+        System.out.println("Luas: " + limas.hitungLuas());
+        System.out.println("Keliling: " + limas.hitungKeliling());
+        System.out.println("Volume: " + limas.hitungVolume());
     }
 }
